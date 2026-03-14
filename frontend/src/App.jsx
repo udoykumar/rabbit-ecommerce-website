a061664232bf95dacfa3c368bc4c168d5febfe37
udoykumar <udoypal.kumar2002@gmail.com>
2026-03-14 06:51:41 +0600

    login and register page complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import UserLayout from "./components/Layout/UserLayout";
 import Home from "./pages/Home";
 import { Toaster } from "sonner";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 const App = () => {
   return (
     <BrowserRouter>
@@ -10,6 +12,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<UserLayout />}>
           <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Route>
       </Routes>
     </BrowserRouter>
